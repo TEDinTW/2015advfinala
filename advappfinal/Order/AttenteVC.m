ac64ea0a39f8422855983927dfbae8930db79226
ios <ios@Mac-13.local>
2015-03-10 21:14:05 +0800

    add tableViews

--- advappfinal/Order/AttenteVC.m
+++ advappfinal/Order/AttenteVC.m
@@ -7,8 +7,13 @@
 //
 
 #import "AttenteVC.h"
-
-@interface AttenteVC ()
+#import "JukunProducts.h"
+@interface AttenteVC ()<UITableViewDataSource,UITableViewDelegate>{
+    JukunProducts *jukunObj;
+    NSInteger _selectRowInTableView1;
+}
+@property (strong, nonatomic) IBOutlet UITableView *tableView1;
+@property (strong, nonatomic) IBOutlet UITableView *tableView2;
 
 @end
 
@@ -17,11 +22,54 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view.
+    _tableView1.dataSource=self;
+    _tableView1.delegate=self;
+    _tableView2.dataSource=self;
+    _tableView2.delegate=self;
+    jukunObj=[[JukunProducts alloc]initWithJsonPath:@"4_order1"];
+
 }
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section{
+
+    if (tableView.tag==1) {
+        return [jukunObj.products count];
+    }else{
+//        NSArray *datas=[[jukunObj.products objectAtIndex:_selectRowInTableView1]objectForKey:@"Menu_List"];
+        return [[[jukunObj.products objectAtIndex:_selectRowInTableView1]objectForKey:@"Menu_List"]count];
+    }
+    
+
+}
+
+// Row display. Implementers should *always* try to reuse cells by setting each cell's reuseIdentifier and querying for available reusable cells with dequeueReusableCellWithIdentifier:
+// Cell gets various attributes set automatically based on table (separators) and data source (accessory views, editing controls)
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath{
+    UITableViewCell *cell;
+//    static NSString* CellID=@"myCellID";
+    cell=[tableView dequeueReusableCellWithIdentifier:@"myCell"];
+//    if (cell==nil) {
+//        cell=[[UITableViewCell alloc]initWithStyle:UITableViewCellStyleDefault reuseIdentifier:CellID];
+//    }
+    if (tableView.tag==1) {
+        cell.textLabel.text=[[jukunObj.products objectAtIndex:indexPath.row]objectForKey:@"Product_Item"];
+    }else{
+        cell.textLabel.text=[[[[jukunObj.products objectAtIndex:_selectRowInTableView1]objectForKey:@"Menu_List"]objectAtIndex:indexPath.row]objectForKey:@"name"];
+    }
+//    cell.textLabel.text=@"aaa";
+    return cell;
+}
+- (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath{
+    if (tableView.tag==1) {
+        _selectRowInTableView1=indexPath.row;
+        [_tableView2 reloadData];
+    }
 }
 
 /*
