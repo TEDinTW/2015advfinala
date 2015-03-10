c3085845afc1d1e1193ca946fe367db26a41813c
ios <ios@Mac-13.local>
2015-03-10 20:38:20 +0800

    add method

--- advappfinal/Order/JukunProducts.h
+++ advappfinal/Order/JukunProducts.h
@@ -9,5 +9,6 @@
 #import <Foundation/Foundation.h>
 
 @interface JukunProducts : NSObject
-
+@property NSArray *products;
+-(id)initWithJsonPath:(NSString *)jsonFile;
 @end
--- advappfinal/Order/JukunProducts.m
+++ advappfinal/Order/JukunProducts.m
@@ -8,23 +8,23 @@
 
 #import "JukunProducts.h"
 @interface JukunProducts()
-{
-    NSMutableArray *products;
-}
+
 @end
 @implementation JukunProducts
--(void)initWithJsonPath:(NSString *)jsonFile{
-
+-(id)initWithJsonPath:(NSString *)jsonFile{
+    if (self=[super init]) {
+        NSString *path = [[NSBundle mainBundle] pathForResource:jsonFile ofType:@"json"];
+        NSData *data = [NSData dataWithContentsOfFile:path];
+        NSArray *jsonObj = [NSJSONSerialization JSONObjectWithData:data options:NSJSONReadingMutableContainers error:nil];
+        self.products=[[NSMutableArray alloc]initWithArray:jsonObj copyItems:YES];
+    }
     
-    NSString *path = [[NSBundle mainBundle] pathForResource:jsonFile ofType:@"json"];
-    NSData *data = [NSData dataWithContentsOfFile:path];
-    NSArray *jsonObj = [NSJSONSerialization JSONObjectWithData:data options:NSJSONReadingMutableContainers error:nil];
-    products=[[NSMutableArray alloc]initWithArray:jsonObj copyItems:YES];
+    return self;
 }
 
 -(NSArray *)getMainProductList{
     NSMutableArray *datas=[[NSMutableArray alloc]init];
-    for (NSDictionary *p in products) {
+    for (NSDictionary *p in self.products) {
         NSString *tmpStr=[p objectForKey:@"Product_Item"];
         [datas addObject:tmpStr];
     }
