7e627673e42b954368dfa6906803d44c49f10826
sky07290921 <ke-hui-qing@MACtekiMacBook-Air.local>
2015-03-12 21:21:44 +0800

    download

---
+++ sky07290921VC.m
@@ -0,0 +1,37 @@
+//
+//  sky07290921VC.m
+//  advappfinal
+//
+//  Created by Ke Hui-Ching on 2015/3/12.
+//  Copyright (c) 2015年 liangjason. All rights reserved.
+//
+
+#import "sky07290921VC.h"
+
+@interface sky07290921VC ()
+
+@end
+
+@implementation sky07290921VC
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
