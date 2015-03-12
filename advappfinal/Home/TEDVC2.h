46efbc981ba92d63821009dc7444d2a10c9696a7
ted <yihsin2010@gmail.com>
2015-03-12 20:07:10 +0800

    added TEDVC2

---
+++ advappfinal/Home/TEDVC2.h
@@ -0,0 +1,13 @@
+//
+//  TEDVC2.h
+//  advappfinal
+//
+//  Created by cheng Yi Hsin on 2015/3/12.
+//  Copyright (c) 2015年 liangjason. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TEDVC2 : UIViewController
+
+@end
