ba76501f724328f6a1d4947b9c4292dd827ca5e3
sky07290921 <sky07290921@gmail.com>
2015-03-14 16:27:24 +0800

    add

--- sky07290921VC.m
+++ sky07290921VC.m
@@ -6,75 +6,6 @@
 ////  Copyright (c) 2015年 liangjason. All rights reserved.
 ////
 //
-//#import "sky07290921VC.h"
-//
-//@interface sky07290921VC ()
-//
-<<<<<<< Updated upstream
-//@end
-//
-//@implementation sky07290921VC
-//
-//- (void)viewDidLoad {
-//    [super viewDidLoad];
-//    // Do any additional setup after loading the view.
-//    
-//    // (1) 讀取 JSON 字串
-//    NSString *path = [[NSBundle mainBundle] pathForResource:@"2_news1" ofType:@"json"];
-//    NSData *data = [NSData dataWithContentsOfFile:path];
-//    // (2) 解析 JSON 字串
-//    news = [NSJSONSerialization JSONObjectWithData:data
-//                                                       options:NSJSONReadingMutableContainers
-//                                                         error:nil];
-//    // (3) 利用迴圈印出解析後的結果
-////    for (NSDictionary *p in news) {
-////        NSString *title = [p objectForKey:@"title"];
-////        NSString *content  = [p objectForKey:@"content"];
-////        NSString *update_time   = [p objectForKey:@"update_time"];
-////        
-////        NSLog(@"標題：%@/n內容：%@/n更新時間：%@", title, content, update_time);
-////    }
-//
-//}
-//
-//- (void)didReceiveMemoryWarning {
-//    [super didReceiveMemoryWarning];
-//    // Dispose of any resources that can be recreated.
-//}
-//
-//- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
-//{
-//    return news.count;
-//}
-//
-//- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
-//{
-//    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"myCell" forIndexPath:indexPath];
-//    cell.textLabel.text = [[news objectAtIndex:indexPath.row] objectForKey:@"title"];
-//    return cell;
-//}
-//
-//- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender
-//{
-//    if ([[segue identifier] isEqualToString:@"DetailVC"]) {
-//        NSIndexPath *indexPath = [self.tableView indexPathForSelectedRow];
-//        [[segue destinationViewController] setDetailItem:[json_array
-//                                                          objectAtIndex:indexPath.row]];
-//    }
-//}
-//
-///*
-//#pragma mark - Navigation
-//
-//// In a storyboard-based application, you will often want to do a little preparation before navigation
-//- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-//    // Get the new view controller using [segue destinationViewController].
-//    // Pass the selected object to the new view controller.
-//}
-//*/
-//
-//@end
-=======
 
 #import "sky07290921VC.h"
 #import "DetailVC.h"
@@ -146,34 +77,7 @@
         [[segue destinationViewController] setDetailItem:[news
                                                           objectAtIndex:indexPath.row]];
     }
-//    //利用index取得陣列內的物件
-//    NSIndexPath *path = [self.tableView indexPathForSelectedRow];
-//    NSString *roleName = [news objectAtIndex:path.row];
-//
-//    //使用segue將物件傳給Detail View Controller class
-//    [segue.destinationViewController setDetailItem:roleName];
-//    if ([[segue identifier] isEqualToString:@"showDetail"]) {
-//        NSIndexPath *indexPath = [self.tableView indexPathForSelectedRow];
-//        NSDictionary *tmpDict = [news objectAtIndex:indexPath.row];
-//        NSManagedObject *object = [[self fetchedResultsController] objectAtIndexPath:indexPath];
-//        //呼叫目的地的ViewController的方法來傳值
-//        [[segue destinationViewController] setDetailItem:object];
-    
-//        [[segue destinationViewController] setData:tmpDict ForKey:@"title"];
-//        [[segue destinationViewController] setDetailItem:[tmpDict objectForKey:@"content"]];
-//        [[segue destinationViewController] setDetailItem:[news objectAtIndex:indexPath.row]];
-//    }
-}
 
-/*
-#pragma mark - Navigation
-
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
 }
-*/
 
 @end
->>>>>>> Stashed changes
