227bcd148ec302569d499fc0986aa8c667c6f21c
chi71524 <ios@Mac-10.local>
2015-03-16 21:48:50 +0800

    commit by chi71524

--- advappfinal/ViewController.m
+++ advappfinal/ViewController.m
@@ -8,10 +8,17 @@
 
 #import "ViewController.h"
 #import "AFHTTPRequestOperationManager.h"
-
+#import "DetailVC.h"
 #import "APPGTool.h"
 #import "TEDVC2.h"
+#import "ZuProduct.h"
+#import "AttenteVC.h"
 @interface ViewController ()
+@property (strong, nonatomic) IBOutlet UIButton *btn0;
+- (IBAction)orderBtn:(UIButton *)sender;
+- (IBAction)productBtn:(UIButton *)sender;
+- (IBAction)newsBtn:(UIButton *)sender;
+- (IBAction)aboutBtn:(UIButton *)sender;
 @property (strong, nonatomic) IBOutlet UIButton *btn1;
 @property (strong, nonatomic) IBOutlet UIButton *btn2;
 @property (strong, nonatomic) IBOutlet UIButton *btn3;
@@ -39,13 +46,14 @@
     self.news.alpha=0;
     self.product.alpha=0;
     self.order.alpha=0;
+    self.btn0.alpha=0;
     self.btn1.alpha = 0;
     self.btn2.alpha = 0;
     self.btn3.alpha = 0;
     self.btn4.alpha = 0;
     self.btn5.alpha = 0;
     self.btn6.alpha = 0;
-    [UIButton animateWithDuration:2.0 animations:^{
+    [UIButton animateWithDuration:6.0 animations:^{
         self.about.alpha=1;
         self.news.alpha=1;
         self.product.alpha=1;
@@ -56,12 +64,13 @@
         self.btn4.alpha=1;
         self.btn5.alpha=1;
         self.btn6.alpha=1;
-        _btn1.frame=CGRectMake(196, 211, 50, 50);
-        _btn2.frame=CGRectMake(196, 211, 50, 50);
-        _btn3.frame=CGRectMake(196, 211, 50, 50);
-        _btn4.frame=CGRectMake(196, 211, 50, 50);
-        _btn5.frame=CGRectMake(196, 211, 50, 50);
-        _btn6.frame=CGRectMake(196, 211, 50, 50);
+        self.btn0.alpha=0.5;
+        _btn1.frame=CGRectMake(196, 211, 70, 70);
+        _btn2.frame=CGRectMake(196, 211, 70, 70);
+        _btn3.frame=CGRectMake(196, 211, 70, 70);
+        _btn4.frame=CGRectMake(196, 211, 70, 70);
+        _btn5.frame=CGRectMake(196, 211, 70, 70);
+        _btn6.frame=CGRectMake(196, 211, 70, 70);
     }];
     AFHTTPRequestOperationManager *manager = [AFHTTPRequestOperationManager manager];
     
@@ -79,4 +88,19 @@
     // Dispose of any resources that can be recreated.
 }
 
+- (IBAction)productBtn:(UIButton *)sender {
+    ZuProduct *productview = [[ZuProduct alloc]initWithNibName:@"ZuProduct" bundle:nil];
+    [self presentViewController:productview animated:YES completion:nil];}
+
+
+- (IBAction)newsBtn:(UIButton *)sender {
+ DetailVC *newsview = [[DetailVC alloc]initWithNibName:@"DetailVC" bundle:nil];
+    [self presentViewController:newsview animated:YES completion:nil];}
+
+- (IBAction)aboutBtn:(UIButton *)sender {
+}
+- (IBAction)orderBtn:(UIButton *)sender {
+  AttenteVC *orderview = [[AttenteVC alloc]initWithNibName:@"AttenteVC" bundle:nil];
+  [self presentViewController:orderview animated:YES completion:nil];
+}
 @end
