ff7ea9346412bf398bf7ac902d5cc35edb135c33
zu <zu@zudeMacBook-Pro.local>
2015-03-16 19:50:00 +0800

    pull

--- advappfinal/Product/zuProductDetail.m
+++ advappfinal/Product/zuProductDetail.m
@@ -11,7 +11,9 @@
 
 @interface zuProductDetail ()
 {
-    NSDictionary *dict;
+    NSDictionary *dict, *dict1;
+    UIButton *imgButton;
+    UIView *upperView;
 //    UILabel *prdNameLabel;
 }
 
@@ -21,54 +23,80 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view.
+    // Do any additional setup after loading the view
     
-   
-    //產品名的文字標籤
-    UILabel *prdNameLabel = [[UILabel alloc]initWithFrame:CGRectMake(45, 76, 285, 36)];//產品名
-    prdNameLabel.text= @"abc";
-//    prdNameLabel.textColor =[UIColor blackColor];
-    
-//    [prdNameLabel objectAtIndex:0];
-  //  lbl.text=[dict objectForKey:@"firstName"];
-    
-//    prdNameLabel.backgroundColor=[UIColor purpleColor];
-    
-    //產品圖
-    UIImageView *prdImg = [[UIImageView alloc]initWithFrame:CGRectMake(36, 156, 303, 303)];
-    prdImg.backgroundColor=[UIColor yellowColor];
-    
-    
-    
-    
-    [self.view addSubview:prdImg];
     
     [self getJson];
-
-    NSArray *prds = [dict objectForKey:@"prd"];
-    NSDictionary *dict1 =[prds objectAtIndex:0];
-
-    NSLog(@"%@",dict1);
+    [self productNameLabel];
+    [self productNameImage];
+    [self productIntroView];
     
-    UILabel * prdNameLabel = [[UILabel alloc]initWithFrame:CGRectMake(45, 76, 285, 36)];
-    prdNameLabel.text=[dict1 objectForKey:@"prdID"];
-    prdNameLabel.backgroundColor = [UIColor redColor];
-    [self.view addSubview:prdNameLabel];
- 
-//    [self getJson];
-    NSLog(@"%@",[dict1 objectForKey:@"prdID"]);
-    
-//    [self getJson];
+
 }
 
 
+//撈JSON
 -(void)getJson{
+
     NSString *filePath=[[NSBundle mainBundle]pathForResource:@"3_product1" ofType:@"json"]; //拿專案裡的檔案
     NSData *data=[NSData dataWithContentsOfFile:filePath];
     NSError *error;
     dict=[NSJSONSerialization JSONObjectWithData:data options:NSJSONReadingMutableContainers error:&error];
-    NSLog(@"%@",[dict objectForKey:@"prdID"]);
+    
+    NSArray *prds = [dict objectForKey:@"prd"];
+    dict1 =[prds objectAtIndex:5];          //第幾個產品 從0開始
+    NSLog(@"%@",dict1);
+    NSLog(@"%@",[dict1 objectForKey:@"prdName"]);
+    NSLog(@"%@",[dict1 objectForKey:@"prdImg"]);
+    NSLog(@"%@",[dict1 objectForKey:@"prdIntro"]);
+ 
 }
+
+//產品名
+-(void)productNameLabel
+{
+    UILabel * prdNameLabel = [[UILabel alloc]initWithFrame:CGRectMake(45, 76, 285, 36)];
+    prdNameLabel.text=[dict1 objectForKey:@"prdName"];
+    prdNameLabel.backgroundColor = [UIColor redColor];
+    [self.view addSubview:prdNameLabel];
+}
+
+
+
+//產品圖
+-(void)productNameImage
+{
+
+    UIImageView *prdImgView = [[UIImageView alloc]initWithFrame:CGRectMake(36, 156, 303, 303)];
+ //   NSString *img=[NSString stringWithFormat:@"flowerfreshbeanflower.png"];
+ //   prdImgView.image=[UIImage imageNamed:img];
+    prdImgView.image=[UIImage imageNamed:[dict1 objectForKey:@"prdImg"]];
+    prdImgView.backgroundColor=[UIColor yellowColor];
+    imgButton = [[UIButton alloc]initWithFrame:CGRectMake(36, 156, 303, 303)];
+    [imgButton addTarget:self action:@selector(productIntroView) forControlEvents:UIControlEventTouchUpInside];
+    
+    [self.view addSubview:prdImgView];
+    [self.view addSubview:imgButton];
+}
+
+-(void)productIntroView
+{
+    upperView = [[UIView alloc]initWithFrame:CGRectMake(0, 0, 375, 667)];
+    upperView.backgroundColor = [UIColor blackColor];
+    upperView.alpha=0.7;
+  //  upperView.hidden=NO;
+    
+    if(upperView.hidden==YES){
+        upperView.hidden=NO;
+        NSLog(@"upperView no hidden");
+    }else{
+        upperView.hidden=YES;
+        NSLog(@"upperView hidden");
+    }
+    [self.view addSubview:upperView];
+
+}
+
 
     
     - (void)didReceiveMemoryWarning {
